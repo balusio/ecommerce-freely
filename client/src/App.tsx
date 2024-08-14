1ad97cececb740cd6f63d4979bd67b01530d80d8
balusio <jorge@trovatrip.com>
2024-08-14 16:49:12 -0300

    Added cors, Product list page and loader of products

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 
+import Root from "./Root";
+import ErrorComponent from "./components/Error";
+import Product from "./components/Product";
+import ProductList, {
+  loader as productsLoader,
+} from "./components/ProductList";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorComponent />,
+    children: [
+      {
+        path: "/",
+        element: <ProductList />,
+        loader: productsLoader,
+      },
+
+      {
+        path: "product/:productId",
+        element: <Product />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ client/src/Root.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/NavBar";
+export default function Root() {
+  return (
+    <>
+      <Navbar />
+      <div className="m-auto max-w-7xl">
+        <div className="sidebar mt-8">
+          <h1>Give Freely Products</h1>
+        </div>
+        <div id="detail">
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
+}
